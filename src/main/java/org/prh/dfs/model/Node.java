ee69ce8f9d8f0a467abc9e566de0f1ebd78ff39b
pranayh24 <pranayheda24@gmail.com>
2025-01-01 17:56:31 +0530

    added Node and HeartbeatMonitor

---
+++ src/main/java/org/prh/dfs/model/Node.java
@@ -0,0 +1,31 @@
+package org.prh.dfs.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Node implements Serializable {
+    private final String nodeId;
+    private final String ipAddress;
+    private final int port;
+    private boolean isAlive;
+    private long lastHeartbeat;
+
+    public Node(String nodeId, String ipAddress, int port) {
+        this.nodeId = nodeId;
+        this.ipAddress = ipAddress;
+        this.port = port;
+        this.isAlive = true;
+        this.lastHeartbeat = System.currentTimeMillis();
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "nodeId='" + nodeId + '\'' +
+                ", port=" + port +
+                ", isAlive=" + isAlive +
+                '}';
+    }
+}
