3eac460fe783365bf7ceb6c21a20011e7e0fba3a
pranayh24 <pranayheda24@gmail.com>
2025-07-05 23:40:24 +0530

    Refactor User model and add UserRepository for JPA integration

--- src/main/java/org/pr/dfs/model/User.java
+++ src/main/java/org/pr/dfs/model/User.java
@@ -1,5 +1,8 @@
 package org.pr.dfs.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +13,12 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="users")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
     private String userId;
     private String username;
     private String email;
---
+++ src/main/java/org/pr/dfs/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.pr.dfs.repository;
+
+import org.pr.dfs.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    User findByUsername(String username);
+}
